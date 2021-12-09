f93a1df53e67d239431366637c0611c256a4f566
Alex Cooksey <aecooksey2651@gmail.com>
2021-12-09 13:47:25 -0500

    commit tree f849861e422681f951fedc21980c1999a0160fe9

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const body = document.querySelector('body')
+const p = document.createElement('p')
+body.append(p)
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail,
+    }
+
+    const postUser = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify(formData),
+    }
+    
+    return fetch('http://localhost:3000/users', postUser)
+        .then(response => response.json())
+        .then(data => {
+            const p = document.createElement('p')
+            body.append(p)
+            p.textContent = data.id})
+        .catch(error => p.textContent = error.message)
+    
+}
